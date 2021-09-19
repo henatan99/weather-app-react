8ece7c20ea47cd8f4ef8cf6659dbec2cbff9d694
Henok <henatan99@gmail.com>
2021-09-19 03:31:41 +0300

    Add scale state to the weatherCard component

--- src/components/weatherCard.js
+++ src/components/weatherCard.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types'; // eslint disable import/no-extraneous-dependencies
 
 const WeatherCard = ({ weather }) => {
   const {
     date, imageUrl, imageName, maxTemp, minTemp,
   } = weather;
+
+  const [scale, setScale] = useState('\xB0C');
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setScale(scale === '\xB0C' ? '\xB0F' : '\xB0C');
+  };
+
   return (
     <div className="weather-card-div">
       <h2 className="weather-card-h2">{date}</h2>
       <img src={imageUrl} alt={imageName} className="weather-card-img" />
       <div className="weather-card-temp-wrapper">
-        <span className="weather-card-temp">{maxTemp}</span>
-        <span className="weather-card-temp">{minTemp}</span>
+        <span className="weather-card-temp">
+          {scale === '\xB0C' ? Math.floor(maxTemp - 273) : Math.floor((maxTemp - 273) * (9 / 5) + 32) }
+        </span>
+        <span className="weather-card-temp">
+          {scale === '\xB0C' ? Math.floor(minTemp - 273) : Math.floor((minTemp - 273) * (9 / 5) + 32) }
+        </span>
+        <button type="button" onClick={handleClick}>{scale}</button>
       </div>
     </div>
   );
